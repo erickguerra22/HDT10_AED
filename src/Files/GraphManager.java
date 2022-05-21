8e30f34154bb5a620aaa76500e52145649b75a02
Diego <diegoguatedb2002@gmail.com>
2022-05-20 19:37:43 -0600

    Documentación agregada

--- src/Files/GraphManager.java
+++ src/Files/GraphManager.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 /**
- * @author erick
+ * Clase GraphManager. Gestiona las operaciones referentes al grafo de distancias entre las diferentes ciudades.
+ * @author Erick Guerra, Diego Morales y Pablo Zamora.
+ * @version 20/05/2022
  *
  */
 public class GraphManager {
@@ -17,6 +19,11 @@ public class GraphManager {
 	private ArrayList<String> vertices = new ArrayList<String>();
 	private String graphCenter = "";
 	
+	/**
+	 * Se encarga de extraer los datos del archivo y los almacena en un formato determinado.
+	 * @param lines Lineas del archivo de texto.
+	 * @throws InvalidGraph
+	 */
 	public void fileToGraph(String[] lines) throws InvalidGraph {
 		for(String l : lines) {
 			String[] line = l.split(" ");
@@ -36,6 +43,10 @@ public class GraphManager {
 		matrizAdyacencias();
 	}
 	
+	/**
+	 * Se encarga de crear la matriz de adyacencia a partir de los vertices y aristas del grafo.
+	 * @throws InvalidGraph
+	 */
 	private void matrizAdyacencias() throws InvalidGraph {
 		Double[][] pesos = new Double[vertices.size()][vertices.size()];
 		for(int i =0; i<vertices.size();i++) {
@@ -62,6 +73,11 @@ public class GraphManager {
 		floyd(pesos);
 	}
 	
+	/**
+	 * Se encarga de ejecutar el algoritmo de floyd para calcular la distancia más corta entre dos nodos.
+	 * @param pesos. Matriz con los pesos de cada una de las aristas.
+	 * @throws InvalidGraph
+	 */
 	private void floyd(Double[][] pesos) throws InvalidGraph{
 		rutas=new HashMap<String, String[]>();
 		ArrayList<String> ruta = new ArrayList<String>();
@@ -99,6 +115,11 @@ public class GraphManager {
 		graphCenter(pesos);
 	}
 	
+	/**
+	 * Se encarga de determinar si los datos almacenados corresponden al formato de un grafo valido.
+	 * @param matriz 
+	 * @return
+	 */
 	private boolean validGraph(Double[][] matriz) {
 		for(Double[] d : matriz) {
 			if(Arrays.asList(d).contains(Double.POSITIVE_INFINITY))
@@ -107,6 +128,11 @@ public class GraphManager {
 		return true;
 	}
 	
+	/**
+	 * Permite determinar la ciudad intermedia del grafo.
+	 * @param ruta Almacena las diferentes rutas entre ciudades.
+	 * @param key Identificador de la ruta.
+	 */
 	private void getIntermediateCities(ArrayList<String> ruta, String key) {
 		if (rutas.containsKey(key)) {
 			String[] info = rutas.get(key);
@@ -116,6 +142,12 @@ public class GraphManager {
 		}
 	}
 	
+	/**
+	 * Permite ejecutar el algoritmo y seleccionar la ruta mas corta entre dos ciudades.
+	 * @param origen. Nombre de la ciudad de origen.
+	 * @param destino. Nombre de la ciudad de destino.
+	 * @return String. Ruta de llegada mas corta.
+	 */
 	public String shorterRoute(String origen, String destino) {
 		String viaje = origen+", "+destino;
 		if(origen.equals(destino))
@@ -129,6 +161,11 @@ public class GraphManager {
 			return "No se encontró una ruta";
 	}
 	
+	/**
+	 * Se encarga de generar un String con cada una de las ciudades intermedias de una ruta.
+	 * @param cities. Arreglo que contiene diferentes nombres de ciudades.
+	 * @return String. Ciudades intermedias.
+	 */
 	private String intermediateCities(String[] cities) {
 		String iCities = "";
 		for(int i = 1;i<cities.length;i++)
@@ -136,6 +173,10 @@ public class GraphManager {
 		return iCities.substring(0, iCities.length()-2);
 	}
 	
+	/**
+	 * Se encarga de calcular el centro del grafo.
+	 * @param pesos. Matriz con los pesos de cada arista.
+	 */
 	public void graphCenter(Double[][] pesos) {
 		Double[] eccentricities = new Double[vertices.size()];
 		for(int i=0;i<vertices.size();i++) {
@@ -156,6 +197,12 @@ public class GraphManager {
 		}
 	}
 	
+	/**
+	 * Permite la eliminación de una arista entre dos nodos (ruta entre dos ciudades).
+	 * @param origen. Ciudad de origen.
+	 * @param destino. Ciudad de destino.
+	 * @return String. Mensaje de respuesta.
+	 */
 	public String breakRoute(String origen, String destino) {
 		String[] ruta = null;
 		String[] inverted = null;
@@ -178,6 +225,14 @@ public class GraphManager {
 			return"No se ha encontrado la ruta especificada.";
 	}
 	
+	/**
+	 * Permite crear nodos (si las ciudades de origen o destino no existen) y establecer relaciones(rutas) entre ellos.
+	 * @param origen. Nombre de la ciudad de origen.
+	 * @param destino. Nombre de la ciudad de destino.
+	 * @param peso. Peso (Km) de la arista.
+	 * @return String. Mensaje de respuesta.
+	 * @throws InvalidGraph
+	 */
 	public String newRoute(String origen, String destino, int peso) throws InvalidGraph {
 		String[] ruta = null;
 		String[] inverted = null;
@@ -228,6 +283,10 @@ public class GraphManager {
 		}
 	}
 	
+	/**
+	 * Metodo getter del centro del grafo.
+	 * @return String. Centro del grafo.
+	 */
 	public String getGraphCenter() {
 		return this.graphCenter;
 	}
